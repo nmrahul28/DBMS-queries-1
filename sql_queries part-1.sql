1a53e42c392fc962c53b45c520085b23b7714f26
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-09-11 16:01:50 +0530

    dbms-1

--- sql_queries part-1.sql
+++ sql_queries part-1.sql
@@ -1,6 +1,6 @@
 -- Problem-1
 SELECT * FROM film INNER JOIN film_category ON film.film_id=film_category.film_id INNER JOIN category ON film_category.category_id=category.category_id WHERE category.name='comedy' AND film.rating='PG-13';
--- Problem-2-- 
+-- Problem-2 
 SELECT film.title ,count(customer.customer_id) AS customer_count from customer INNER JOIN rental ON customer.customer_id= rental.customer_id INNER JOIN inventory ON rental.inventory_id=inventory.inventory_id INNER JOIN film ON inventory.film_id=film.film_id INNER JOIN film_category ON film.film_id=film_category.film_id INNER JOIN category ON category.category_id=film_category.category_id WHERE category.name="horror" GROUP BY film.film_id ORDER BY customer_count DESC LIMIT 3;
 -- Problem-3
 SELECT customer.first_name, customer.last_name, category.name, country.country FROM customer INNER JOIN address ON customer.address_id=address.address_id INNER JOIN  city ON address.city_id=city.city_id INNER JOIN country ON city.country_id=country.country_id INNER JOIN rental ON rental.customer_id=customer.customer_id INNER JOIN inventory ON inventory.inventory_id=rental.inventory_id INNER JOIN film_category ON film_category.film_id=inventory.film_id INNER JOIN category ON category.category_id=film_category.category_id WHERE country.country='india' and category.name='sports';
